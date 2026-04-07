caa33cd0241e9e006d4bcafd6d178acbcb276a27
venkat <telagamreddyvenkat@gmail.com>
2026-04-07 08:22:29 +0530

    Time: 58 ms (97.2%), Space: 61.2 MB (5.59%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -0,0 +1,32 @@
+class Robot {
+    private int p, w, h;
+
+    public Robot(int width, int height) {
+        w = width - 1;
+        h = height - 1;
+        p = 0;
+    }
+
+    public void step(int num) {
+        p += num;
+    }
+
+    public int[] getPos() {
+        int remain = p % (2 * (w + h));
+        if (remain <= w) return new int[]{remain, 0};
+        remain -= w;
+        if (remain <= h) return new int[]{w, remain};
+        remain -= h;
+        if (remain <= w) return new int[]{w - remain, h};
+        remain -= w;
+        return new int[]{0, h - remain};
+    }
+
+    public String getDir() {
+        int[] pos = getPos();
+        if (p == 0 || (pos[1] == 0 && pos[0] > 0)) return "East";
+        if (pos[0] == w && pos[1] > 0) return "North";
+        if (pos[1] == h && pos[0] < w) return "West";
+        return "South";
+    }
+}
